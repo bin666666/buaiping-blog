90d57e6f0b826dd8d23493a16a53bba5cb10b14d
gitbu <1282185668@qq.com>
2016-01-17 11:24:31 +0800

    第一次提交

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var db=require('./db'); //导入db模块
 var app = express();
 
 // view engine setup
@@ -16,6 +16,21 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+var settings = require('./settings');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+app.use(session({
+    secret: settings.cookieSecret,//secret 用来防止篡改 cookie
+    key: settings.db,//key 的值为 cookie 的名字
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//设定 cookie 的生存期，这里我们设置 cookie 的生存期为 30 天
+    resave:true,
+    saveUninitialized:true,
+    store: new MongoStore({ //设置它的 store 参数为 MongoStore 实例，把会话信息存储到数据库中，以避免重启服务器时会话丢失
+      db: settings.db,
+      host: settings.host,
+      port: settings.port,
+    })
+}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,14 +39,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use(function(req,res,next){
+    res.locals.user = req.session.user;
+    res.locals.success = req.flash('success').toString();
+    res.locals.error = req.flash('error').toString();
+    next();
+});
+var accessLog = fs.createWriteStream('access.log', {flags: 'a'});
+app.use(logger('dev',{stream: accessLog}));
 
+//错误日志
+var errorLog = fs.createWriteStream('error.log', {flags: 'a'});
+app.use(function (err, req, res, next) {
+    var meta = '[' + new Date() + '] ' + req.url + '\n';
+    errorLog.write(meta + err.stack + '\n');
+    next();
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    res.render("404");
 });
-
 // error handlers
 
 // development error handler
@@ -45,16 +72,6 @@ if (app.get('env') === 'development') {
     });
   });
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+/*router.get('/', function(req, res, next) {
+  Model('Article').find({}).populate('user').exec(function(err,articles){
+    res.render('index', {title: '主页',articles:articles});
+  });
+});*/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/articles/list/1/2');
 });
 
 module.exports = router;
