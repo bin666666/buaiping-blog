9f81d02a2b5c808fe280a902af53883e9b3ba319
gitbu <1282185668@qq.com>
2016-01-17 14:21:13 +0800

    静态页面

--- app.js
+++ app.js
@@ -1,77 +1,64 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var path = require('path');//处理路径
+var favicon = require('serve-favicon');//处理收藏夹图标的
+var logger = require('morgan');//处理日志
+var cookieParser = require('cookie-parser');//处理cookie req.cookie req.cookies
+var bodyParser = require('body-parser');//解析请求体的
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var db=require('./db'); //导入db模块
+var routes = require('./routes/index');//根路由
+var users = require('./routes/users');//用户路由
+var articles = require('./routes/articles');//文章路由
+
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));//设置模板的存放路径
+app.set('view engine', 'html');//设置模板引擎
+app.engine('html',require('ejs').__express);//设置对html文件的渲染方式
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-var settings = require('./settings');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
-app.use(session({
-    secret: settings.cookieSecret,//secret 用来防止篡改 cookie
-    key: settings.db,//key 的值为 cookie 的名字
-    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//设定 cookie 的生存期，这里我们设置 cookie 的生存期为 30 天
-    resave:true,
-    saveUninitialized:true,
-    store: new MongoStore({ //设置它的 store 参数为 MongoStore 实例，把会话信息存储到数据库中，以避免重启服务器时会话丢失
-      db: settings.db,
-      host: settings.host,
-      port: settings.port,
-    })
-}));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));//指定日志输出的格式
+app.use(bodyParser.json());//处理JSON 通过 Content-Type来判断是否由自己来处理
+app.use(bodyParser.urlencoded({ extended: false }));//处理form-urlencoded
+app.use(cookieParser());//处理cookie 把请求头中的cookie转成对象，加入一个cookie函数的属性
+app.use(express.static(path.join(__dirname, 'public')));//静态文件服务
 
 app.use('/', routes);
 app.use('/users', users);
-app.use(function(req,res,next){
-    res.locals.user = req.session.user;
-    res.locals.success = req.flash('success').toString();
-    res.locals.error = req.flash('error').toString();
-    next();
-});
-var accessLog = fs.createWriteStream('access.log', {flags: 'a'});
-app.use(logger('dev',{stream: accessLog}));
+app.use('/articles', articles);
 
-//错误日志
-var errorLog = fs.createWriteStream('error.log', {flags: 'a'});
-app.use(function (err, req, res, next) {
-    var meta = '[' + new Date() + '] ' + req.url + '\n';
-    errorLog.write(meta + err.stack + '\n');
-    next();
-});
 // catch 404 and forward to error handler
+// 捕获404错误并且转发到错误处理中间件
 app.use(function(req, res, next) {
-    res.render("404");
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
+    res.status(err.status || 500);//设置响应状态码
+    res.render('error', { //渲染模板 模板文件 参数数据
       message: err.message,
       error: err
     });
   });
 }
+
+// production error handler 生成环境下的错误昝
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
 var express = require('express');
+//返回一个路由的实例
 var router = express.Router();
 
 /* GET home page. */
-/*router.get('/', function(req, res, next) {
-  Model('Article').find({}).populate('user').exec(function(err,articles){
-    res.render('index', {title: '主页',articles:articles});
-  });
-});*/
 router.get('/', function(req, res, next) {
-  res.redirect('/articles/list/1/2');
+  res.render('index', { title: '欢迎光临我的博客' });
 });
-
 module.exports = router;
